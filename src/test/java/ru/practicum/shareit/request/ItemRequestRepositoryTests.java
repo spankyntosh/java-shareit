4212f3c4b7205c0652a89c5d05ecc29a679f7c5f
uskov vladimir <spankynton1@yandex.ru>
2023-05-19 22:48:28 +0300

    Добавление тестов по контроллерам

--- src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTests.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTests.java
@@ -1,14 +1,11 @@
 package ru.practicum.shareit.request;
 
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.jdbc.Sql;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
 
